aaaeeaff84d232776557ede0c651e30f6de1363f
Vladymyr Cherno <107193515+irohtea@users.noreply.github.com>
2022-11-07 21:00:26 +0200

    added vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/to-do-list/'
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/to-do-list/' :
+    '/'
 })
